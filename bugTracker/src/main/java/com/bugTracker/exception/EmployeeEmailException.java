3c08abec50067d818afa7343a8209dccd4dce971
Manish Rawat <85739547+manish098rawat@users.noreply.github.com>
2022-07-19 09:10:49 +0530

    Add files via upload

---
+++ bugTracker/src/main/java/com/bugTracker/exception/EmployeeEmailException.java
@@ -0,0 +1,20 @@
+package com.bug.tracking.execption;
+
+public class EmployeeEmailException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EmployeeEmailException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmployeeEmailException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ bugTracker/src/main/java/com/bugTracker/exception/UserEmailException.java
@@ -0,0 +1,20 @@
+package com.bug.tracking.execption;
+
+public class UserEmailException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UserEmailException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserEmailException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
