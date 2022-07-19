71921058f30d640ca405a17a6f68539b880b6ec5
Manish Rawat <85739547+manish098rawat@users.noreply.github.com>
2022-07-19 09:11:58 +0530

    Add files via upload

--- bugTracker/src/main/java/com/bugTracker/repository/EmployeeRepo.java
+++ bugTracker/src/main/java/com/bugTracker/repository/EmployeeRepo.java
@@ -1,9 +1,14 @@
 package com.bug.tracking.repository;
 
 import com.bug.tracking.model.*;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+
+	void deleteById(Optional<Employee> user);
 }
--- bugTracker/src/main/java/com/bugTracker/repository/UserRepo.java
+++ bugTracker/src/main/java/com/bugTracker/repository/UserRepo.java
@@ -1,9 +1,12 @@
 package com.bug.tracking.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bug.tracking.model.User;
 
 public interface UserRepo extends JpaRepository<User, Long> {
+	void deleteById(Optional<User> user);
 
 }
