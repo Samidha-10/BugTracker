1110b0a2970bae2a007a1d8e1b0705d03303c09a
Samidha <98950177+Samidha-10@users.noreply.github.com>
2022-07-20 12:00:56 +0530

    Update IBugRepository.java

--- bugTracker/src/main/java/com/bugTracker/project/repository/IBugRepository.java
+++ bugTracker/src/main/java/com/bugTracker/project/repository/IBugRepository.java
@@ -1,5 +1,22 @@
 package com.bugTracker.project.repository;
 
-public class IBugRepository {
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bugTracker.project.modal.Bug;
+
+
+@Repository
+public interface IBugRepository extends CrudRepository<Bug, Integer> {
+
+	Optional<Bug> findById(long id);
+
+	void deleteById(long id);
+
+	List<Bug> findByStatus(String status);
 
 }
