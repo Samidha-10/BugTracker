8c06dd75e95ccf02e842d8e5ab676110d73501ff
ritik1022 <55057989+ritik1022@users.noreply.github.com>
2022-07-24 02:39:04 +0530

    Update BugRepository.java

--- buggtracking-main/src/main/java/com/bug/tracking/repository/BugRepository.java
+++ buggtracking-main/src/main/java/com/bug/tracking/repository/BugRepository.java
@@ -1,5 +1,7 @@
 package com.bug.tracking.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,7 +9,8 @@ import com.bug.tracking.modal.Bug;
 
 @Repository
 public interface BugRepository extends CrudRepository<Bug, Long>{
-
+	List<Bug> findAll();
+	List<Bug> findByStatus(String status);
 	Bug findBugById(Long id);
 
 }
