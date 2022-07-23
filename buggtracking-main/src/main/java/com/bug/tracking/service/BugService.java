51d3db80935fe407510e3f98611c76d14e102c70
ritik1022 <55057989+ritik1022@users.noreply.github.com>
2022-07-24 02:37:12 +0530

    Update BugService.java

--- buggtracking-main/src/main/java/com/bug/tracking/service/BugService.java
+++ buggtracking-main/src/main/java/com/bug/tracking/service/BugService.java
@@ -1,11 +1,16 @@
 package com.bug.tracking.service;
 
+import java.util.List;
+
+import com.bug.tracking.exception.BugIdException;
 import com.bug.tracking.modal.Bug;
 
 public interface BugService {
 
-	public Bug saveOrUpdate(Bug bug);
-	public Bug findBugById(Long id);
-	public Iterable<Bug> findAllBug();
-	public void deleteBugById(Long id);
+	public Bug createBug(Bug bug);
+	public Bug updateBug(Bug bug ) throws BugIdException;
+	public Bug findBugById(Long bugId) throws BugIdException;
+	public List<Bug> findAllBug();
+	public List<Bug> getAllBugsByStatus(String Status);
+	public void deleteBugById(Long id) throws BugIdException;
 }
